fa7d7c61011f8cab16e1efe424bfcda7dabaa057
Álvaro León García <alg201@alu.ua.es>
2024-11-09 17:25:41 +0100

    parece terminado

---
+++ script.js
@@ -0,0 +1,23 @@
+const initialText = "StartPage";
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function writtingEffect(){
+    let text = "<span class='blinking-text'>_</span>";
+    let title = document.getElementById("title");
+    title.innerHTML += text;
+    for (const char of initialText) {
+        console.log(char)
+        title.innerText = title.innerText.replace("_", char);
+        title.innerText += "_";
+        await sleep(100);
+    }
+    title.innerText = title.innerText.replace("_","");
+    title.innerHTML += "<span class='blinking-text'>_</span>"
+}
+
+function startup(){
+    writtingEffect();
+}
